47f880a00951fab515f3f6f53fab4eec778d881e
mirocow <mr.mirocow@gmail.com>
2018-10-15 16:18:11 +0300

    first commit

---
+++ src/ElasticsearchTarget.php
@@ -0,0 +1,146 @@
+<?php
+namespace mirocow\elasticsearch\log;
+
+use Yii;
+use yii\di\Instance;
+use yii\elasticsearch\Connection;
+use yii\helpers\Json;
+use yii\helpers\VarDumper;
+use yii\log\Logger;
+use yii\log\Target;
+
+/**
+ * Elasticsearch Yii2 Log Target
+ *
+ * @author Mirocow <mr.mirocow@gmail.com>
+ * @since 2.0
+ */
+class ElasticsearchTarget extends Target
+{
+    /**
+     * @var string Elasticsearch index name
+     */
+    public $index = 'yii';
+
+    /**
+     * @var string Elasticsearch type name
+     */
+    public $type = 'log';
+
+    /**
+     * @var LogTargetIndex
+     */
+    public $db;
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+        $this->db = IndexerFactory::createIndex(LogTargetIndex::class);
+        $this->db->create(true);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function export()
+    {
+        $messages = array_map([$this, 'prepareMessage'], $this->messages);
+
+        $body = implode("\n", $messages) . "\n";
+
+        /** @var QueryBuilder $query */
+        $query = $this->db->search($body, 'bulk');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function collect($messages, $final)
+    {
+        $this->messages = array_merge($this->messages, static::filterMessages($messages, $this->getLevels(), $this->categories, $this->except));
+        $count = count($this->messages);
+        if ($count > 0 && ($final || $this->exportInterval > 0 && $count >= $this->exportInterval)) {
+            // set exportInterval to 0 to avoid triggering export again while exporting
+            $oldExportInterval = $this->exportInterval;
+            $this->exportInterval = 0;
+            $this->export();
+            $this->exportInterval = $oldExportInterval;
+
+            $this->messages = [];
+        }
+    }
+
+    /**
+     * Prepares a log message
+     * @param array $message The log message to be formatted
+     * @return string
+     */
+    public function prepareMessage($message)
+    {
+        list($text, $level, $category, $timestamp) = $message;
+
+        $result = [
+            'category' => $category,
+            'level' => Logger::getLevelName($level),
+            '@timestamp' => date('c', $timestamp),
+        ];
+
+        if (isset($message[4])) {
+            $result['trace'] = $message[4];
+        }
+
+        if ($text instanceof \Exception) {
+            $result['message'] = $text->getMessage();
+            $result['exception'] = [
+                'file' => $text->getFile(),
+                'line' => $text->getLine(),
+                'code' => $text->getCode(),
+                'trace' => $text->getTraceAsString(),
+            ];
+        } elseif (is_string($text)) {
+            $result['message'] = $text;
+        } else {
+            $result['message'] = VarDumper::export($text);
+        }
+
+        $result = array_merge($result, $this->getExtraFields());
+
+        $message = implode("\n", [
+            Json::encode([
+                'index' => new \stdClass()
+            ]),
+            Json::encode($result)
+        ]);
+
+        return $message;
+    }
+
+    private $_extraFields = [];
+
+    /**
+     * Returns extra fields
+     * @return array
+     */
+    public function getExtraFields()
+    {
+        return $this->_extraFields;
+    }
+
+    /**
+     * Set extra fields
+     * @param array $fields
+     */
+    public function setExtraFields(array $fields)
+    {
+        foreach ($fields as $name => $field) {
+            if ($field instanceof \Closure || is_array($field) && is_callable($field)) {
+                $this->_extraFields[$name] = call_user_func($field, Yii::$app);
+            } else {
+                $this->_extraFields[$name] = $field;
+            }
+        }
+    }
+}
